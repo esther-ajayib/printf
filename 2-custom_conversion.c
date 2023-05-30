33bd2d21cf2cf7dfef4322942b9ad22a2929facf
Esther <estherajayibunmi@gmail.com>
2023-05-30 08:42:35 -0700

    just commit

--- 2-custom_conversion.c
+++ 2-custom_conversion.c
@@ -4,117 +4,125 @@
 
 /**
  * _printf - Produces output according to a format.
- * @format: A character string containing zero or more directives.
+ * @format: The format string
  *
- * Return: The number of characters printed (excluding the null byte used to end output to strings).
+ * Return: The number of characters printed (excluding the null byte)
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    int num;
-    unsigned int u_num;
-    char c;
-    char *str;
+	va_list args;
+	int count = 0;
+	char *str;
+	int num;
+	unsigned int u_num;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            write(1, format, 1);
-            count++;
-        }
-        else
-        {
-            format++;
-            switch (*format)
-            {
-                case 'c':
-                    c = (char)va_arg(args, int);
-                    write(1, &c, 1);
-                    count++;
-                    break;
-                case 's':
-                    str = va_arg(args, char *);
-                    while (*str)
-                    {
-                        write(1, str, 1);
-                        str++;
-                        count++;
-                    }
-                    break;
-                case '%':
-                    write(1, "%", 1);
-                    count++;
-                    break;
-                case 'd':
-                case 'i':
-                    num = va_arg(args, int);
-                    if (num < 0)
-                    {
-                        write(1, "-", 1);
-                        count++;
-                        num = -num;
-                    }
-                    count += print_number(num);
-                    break;
-                case 'b':
-                    u_num = va_arg(args, unsigned int);
-                    count += print_binary(u_num);
-                    break;
-                default:
-                    write(1, format - 1, 2);
-                    count += 2;
-                    break;
-            }
-        }
+	while (format && *format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					_putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (str == NULL)
+						str = "(null)";
+					while (*str)
+					{
+						_putchar(*str);
+						str++;
+						count++;
+					}
+					break;
+				case '%':
+					_putchar('%');
+					count++;
+					break;
+				case 'd':
+				case 'i':
+					num = va_arg(args, int);
+					count += print_number(num);
+					break;
+				case 'b':
+					u_num = va_arg(args, unsigned int);
+					count += print_binary(u_num);
+					break;
+				default:
+					_putchar('%');
+					count++;
+					_putchar(*format);
+					count++;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+		format++;
+	}
 
-        format++;
-    }
+	va_end(args);
 
-    va_end(args);
-
-    return count;
+	return (count);
 }
 
 /**
- * print_number - Prints a number to stdout.
- * @num: The number to be printed.
+ * print_number - Prints a number
+ * @num: The number to print
  *
- * Return: The number of characters printed.
+ * Return: The number of characters printed
  */
 int print_number(int num)
 {
-    int count = 0;
+	int count = 0;
+	int divisor = 1;
 
-    if (num / 10)
-        count += print_number(num / 10);
+	if (num < 0)
+	{
+		_putchar('-');
+		count++;
+		num *= -1;
+	}
 
-    char digit = '0' + (num % 10);
-    write(1, &digit, 1);
-    count++;
+	while (divisor <= num / 10)
+		divisor *= 10;
 
-    return count;
+	while (divisor >= 1)
+	{
+		_putchar('0' + (num / divisor));
+		count++;
+		num %= divisor;
+		divisor /= 10;
+	}
+
+	return (count);
 }
 
 /**
- * print_binary - Prints an unsigned integer as binary to stdout.
- * @num: The unsigned integer to be printed as binary.
+ * print_binary - Prints an unsigned integer in binary format
+ * @num: The unsigned integer to print in binary
  *
- * Return: The number of characters printed.
+ * Return: The number of characters printed
  */
 int print_binary(unsigned int num)
 {
-    int count = 0;
+	int count = 0;
+	unsigned int mask = 1 << ((sizeof(unsigned int) * 8) - 1);
 
-    if (num / 2)
-        count += print_binary(num / 2);
+	while (mask)
+	{
+		_putchar((num & mask) ? '1' : '0');
+		count++;
+		mask >>= 1;
+	}
 
-    char digit = '0' + (num % 2);
-    write(1, &digit, 1);
-    count++;
-
-    return count;
+	return (count);
 }
--- main.h
+++ main.h
@@ -6,5 +6,7 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_number(int num);
+int print_binary(unsigned int num);
 
 #endif /* MAIN_H */
