91a91ce5239f2b3f4533c28f907d870ca444e9a4
Esther <estherajayibunmi@gmail.com>
2023-05-31 09:08:35 -0700

    task6

---
+++ 6-conversion_specifier.c
@@ -0,0 +1,72 @@
+#include "main.h"
+
+/**
+ * print_address - Prints the address of a pointer
+ * @args: The va_list argument containing the pointer
+ *
+ * Return: The number of characters printed
+ */
+int print_address(va_list args)
+{
+    void *ptr = va_arg(args, void *);
+
+    if (!ptr)
+        return _printf("(nil)");
+
+    return _printf("0x%lx", (unsigned long)ptr);
+}
+
+/**
+ * get_custom_specifier - Retrieves the corresponding print function for a custom specifier
+ * @specifier: The custom specifier character
+ *
+ * Return: A pointer to the corresponding print function, or NULL if not found
+ */
+int (*get_custom_specifier(char specifier))(va_list)
+{
+    if (specifier == 'p')
+        return print_address;
+
+    return NULL;
+}
+
+/**
+ * _printf - Prints formatted output to stdout
+ * @format: The format string
+ *
+ * Return: The number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int i = 0, count = 0;
+    int (*f)(va_list);
+
+    if (format == NULL)
+        return -1;
+
+    va_start(args, format);
+
+    while (format && format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            f = get_custom_specifier(format[i]);
+            if (f != NULL)
+                count += f(args);
+            else
+                count += _putchar('%') + _putchar(format[i]);
+        }
+        else
+        {
+            count += _putchar(format[i]);
+        }
+        i++;
+    }
+
+    va_end(args);
+
+    return count;
+}
+
--- main.h
+++ main.h
@@ -7,6 +7,7 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_number(unsigned int n, int base, int uppercase);
+int print_address(va_list args);
 int print_binary(unsigned int num);
 
 #endif /* MAIN_H */
