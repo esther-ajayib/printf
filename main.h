9ce8937d1459feb85ac32a4d4c83c2f29888e4be
Esther <estherajayibunmi@gmail.com>
2023-05-31 08:06:36 -0700

    Revert "task 2"
    This reverts commit 8a5ffc1061dbf3b44e2f58ace781cd486d5923f4.

--- main.h
+++ main.h
@@ -6,7 +6,11 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+<<<<<<< HEAD
 int print_number(unsigned int n, int base, int uppercase);
+=======
+int print_number(int n);
+>>>>>>> parent of 8a5ffc1... task 2
 int print_binary(unsigned int num);
 
 #endif /* MAIN_H */
